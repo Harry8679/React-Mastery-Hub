9a92a02a0ab7ef148f4e51073af43c6555d6574d
Harry7986 <emarh.harry.code@gmail.com>
2025-12-14 17:37:28 +0100

    Theme Context Project 3

--- src/context/ThemeContextProject.tsx
+++ src/context/ThemeContextProject.tsx
@@ -1,4 +1,5 @@
-import { useState, createContext, useContext, ReactNode } from 'react';
+import { useState, createContext, useContext } from 'react';
+import type { ReactNode } from 'react';
 import { ChevronLeft, Code2, Sun, Moon } from 'lucide-react';
 import { ProjectComponentProps } from '../../types';
 
